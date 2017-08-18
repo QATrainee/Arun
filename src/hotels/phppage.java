61f71f5e11d7a387cb52b0203ca23234424a09a9
lakshmi <training@tiliconveli.com>
2017-08-18 15:07:09 +0530

    a

--- src/hotels/phppage.java
+++ src/hotels/phppage.java
@@ -22,11 +22,11 @@ driver.get("http://www.phptravels.net");
 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 driver.findElement(By.cssSelector(".select2-chosen")).click();
 driver.findElement(By.cssSelector(".select2-input.select2-focused")).sendKeys("Dub");
-Thread.sleep(2000);
+driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 driver.findElement(By.xpath(".//*[@id='select2-drop']/ul/li/ul/li[1]/div")).click();
-Thread.sleep(2000);
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 driver.findElement(By.cssSelector(".form.input-lg.dpd1")).click();
-Thread.sleep(2000);
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 String ExpMonth = "November 2017";
 driver.findElement(By.xpath("html/body/div[13]/div[1]/table/thead/tr[1]/th[3]")).click();
 String CurrMonth=driver.findElement(By.xpath("html/body/div[13]/div[1]/table/thead/tr[1]/th[2]")).getText();
@@ -36,7 +36,7 @@ System.out.println("Already Selected");
 else {
 for(int i=1;i<12;i++) {
 driver.findElement(By.xpath("html/body/div[13]/div[1]/table/thead/tr[1]/th[3]")).click();
-Thread.sleep(2000);
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 CurrMonth=driver.findElement(By.xpath("html/body/div[13]/div[1]/table/thead/tr[1]/th[2]")).getText();
 if (ExpMonth.equals(CurrMonth)) {
 System.out.println("Month Selected");
@@ -44,12 +44,12 @@ break;
 }
 }
 }
-Thread.sleep(2000);
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 WebElement datepicker = driver.findElement(By.xpath("html/body/div[13]/div[1]/table/tbody/tr[3]/td[3]"));
 datepicker.click();
-Thread.sleep(2000);
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 driver.findElement(By.cssSelector(".form.input-lg.dpd2")).clear();
-Thread.sleep(2000);
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 String ExpsMonth = "December 2017";
 driver.findElement(By.xpath("html/body/div[14]/div[1]/table/thead/tr[1]/th[3]")).click();
 String CurrsMonth=driver.findElement(By.xpath("html/body/div[14]/div[1]/table/thead/tr[1]/th[2]")).getText();
@@ -59,7 +59,7 @@ System.out.println("Already Selected");
 else {
 for(int i=1;i<12;i++) {
 driver.findElement(By.xpath("html/body/div[14]/div[1]/table/thead/tr[1]/th[3]")).click();
-Thread.sleep(2000);
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 CurrsMonth=driver.findElement(By.xpath("html/body/div[14]/div[1]/table/thead/tr[1]/th[2]")).getText();
 if (ExpsMonth.equals(CurrsMonth)) {
 System.out.println("Month Selected");
@@ -67,19 +67,19 @@ break;
 }
 }
 }
-Thread.sleep(2000);
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 WebElement datepicker1 = driver.findElement(By.xpath("html/body/div[14]/div[1]/table/tbody/tr[3]/td[5]"));
 datepicker1.click();
-Thread.sleep(3000);
+driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 driver.findElement(By.id("adults")).click();
-Thread.sleep(2000);
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 new Select(driver.findElement(By.id("adults"))).selectByVisibleText("4");
 //driver.findElement(By.cssSelector("#adults > option[value=\"4\"]")).click();
 driver.findElement(By.xpath("//button[@type='submit']")).click();
 driver.findElement(By.xpath("//h4/a/b")).click();
 for(int i=0;i<10;i++) {
 driver.findElement(By.xpath(".//*[@id='OVERVIEW']/div/div[2]/div[1]/div[3]/div[2]/div/div[1]/div[3]")).click();
-Thread.sleep(1000);
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 }
 driver.quit();
 }
